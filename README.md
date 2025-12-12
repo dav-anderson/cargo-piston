54b5ac7c6d09750532cae63f83530e57e2e703f5
dav-anderson <david@orange.me>
2025-12-12 16:12:32 -0500

    fixed an issue with reading the cargo.toml, there is a bug in the resources embed portion of the configure_bundle script

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 ##Configure your .ENV & Cargo.toml
 This app requires a .ENV file to work properly in most cases. In order to build your apps for a desired platform, certain configurations will be required in your .ENV and as well in the `Cargo.toml` of the project in which you wish to utilize `cargo-piston`
 
+##Cargo.toml configuration
+You can provide the absolute path to an icon .png file with 
+`icon_path = "absolute/path/to/icon.png`
+
 ##Supported Build Targets
 *List supported build targets here*
 
@@ -9,4 +13,14 @@ This app requires a .ENV file to work properly in most cases. In order to build 
 Run the following command within your rust project repo to install the package locally
 `cargo install --path .`
 
+##Compiling for windows
+
+install mingw32
+
+#App Icon
+You must have embed-resource in your Cargo.toml as a `[build dependency]`
+```
+[build dependency]
+embed-resource = "3.0.2"
+```
 
--- src/windows.rs
+++ src/windows.rs
@@ -1,8 +1,11 @@
-use cargo_metadata::{Metadata, MetadataCommand};
+use cargo_metadata::{Metadata, MetadataCommand, DependencyKind};
 use anyhow::{Context, bail, Result};
 use std::env;
-use std::path::PathBuf;
-use std::fs::{create_dir_all, write};
+use std::path::{Path, PathBuf};
+use std::fs::{create_dir_all, write, remove_file};
+use std::io::Write;
+use std::fs::File;
+use image::{self, imageops, DynamicImage, ImageEncoder};
 use crate::helper::Helper;
 
 
@@ -40,25 +43,29 @@ impl WindowsBuilder {
             .exec() {
                 Ok(md) => md,
                 Err(_) => bail!("error parsing cargo toml")
-            };
+        };
 
+        // check if embed resources is installed
+        let embed_resources_ok: bool = if let Some(root_pkg) = metadata.root_package() {
+            root_pkg.dependencies.iter().any(|dep| dep.name == "embed-resource" && dep.kind == DependencyKind::Build)
+        } else {
+            false
+        };
+        println!("Embed Resources Installed: {}", embed_resources_ok);
         // Read standard fields from the first package
-        if let Some(package) = metadata.packages.first() {
+        if let Some(package) = metadata.root_package() {
             println!("Package name: {}", package.name);
             println!("Version: {}", package.version);
-            // //Example Read dependencies (example: check if "clap" is a dep)
-            // if let Some(dep) = package.dependencies.iter().find(|d| d.name == "clap") {
-            //     println!("Clap dependency version req: {}", dep.req);
-            // }
             // Read custom [package.metadata] keys (if present)
             if let serde_json::Value::Object(meta) = &package.metadata {
+                println!("testing***************");
                 //parse icon_path from the cargo.toml
                 if let Some(icon_path) = meta.get("icon_path") {
                     println!("icon_path: {}", icon_path);
-                    self.configure_bundle(Some(icon_path.to_string()));
+                    self.configure_bundle(Some(icon_path.to_string()), embed_resources_ok);
                 }else {
                     println!("no icon path found");
-                    self.configure_bundle(None);
+                    self.configure_bundle(None, embed_resources_ok);
                 }
             }
         } else {
@@ -67,7 +74,7 @@ impl WindowsBuilder {
         Ok(())
     }
 
-    fn configure_bundle(&mut self, icon_path: Option<String>) -> Result <()>{
+    fn configure_bundle(&mut self, icon_path: Option<String>, embed_resources_ok: bool) -> Result <()>{
         println!("building the dynamic app bundle");
         let cwd: PathBuf = self.cwd.clone();
         println!("working dir: {:?}", cwd);
@@ -92,11 +99,52 @@ impl WindowsBuilder {
         //TODO open question: Will the App.rc compiling break the bundle if the user does not provide an icon?
         println!("Icon path: {}", &icon_path.clone().unwrap());
         //if no icon was provided
-        if icon_path.is_none() {
-            return Ok(())
-        //if icon was provided, format the app icon for windows
-        }else {
-        //TODO convert the .png at the icon_path to a .ico which resides in the app bundle
+        if !icon_path.is_none() && embed_resources_ok{
+            println!("icon path provided and embed resources installed, configuring icon");
+            //TODO convert the .png at the icon_path to a .ico which resides in the app bundle
+            let icon_output: PathBuf = cwd.join(rc_icon);
+            println!("icon output path: {}", icon_output.display());
+            //TODO SOMETHING IS BREAKING BEYOND THIS LINE AND NOT GETTING HANDLED
+            let img = image::open(Path::new(&icon_path.clone().unwrap()))?;
+            // Resize to the specified size
+            let resized = imageops::resize(&img, 64, 64, imageops::FilterType::Lanczos3);
+            let resized_img = DynamicImage::ImageRgba8(resized);
+            let file = std::fs::File::create(icon_output.clone())?;
+            let mut writer = std::io::BufWriter::new(file);
+            let encoder = image::codecs::ico::IcoEncoder::new(&mut writer);
+            encoder
+                .write_image(
+                    resized_img.as_bytes(),
+                    64,
+                    64,
+                    image::ExtendedColorType::Rgba8,
+                )?;
+            println!("Converted {} to ICO ({}x{}) and saved as {}",icon_path.unwrap(), 64, 64, icon_output.display());
+            let build_path: PathBuf = cwd.join("build.rs");
+            //if a build.rs file exists, first remove it.
+            if build_path.exists() {
+                remove_file(&build_path).context("failed to remove existing build.rs")?;
+            }
+            //populate the build.rs content
+            let build_content = format!(
+                r#"
+                use std::io;
+
+                fn main() {{
+                    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" && std::path::Path::new("{}").exists() {{
+                        embed_resource::compile("app.rc", embed_resource::NONE)
+                        .manifest_optional();
+                    }}
+            }}
+            
+                "#,
+                &icon_output.display()
+            );
+            //Generate a build.rs file
+            let mut build_file = File::create(&build_path)?;
+            build_file.write_all(build_content.as_bytes())?;
+            build_file.flush()?;
+            println!("Created Build.rs at {}", &build_path.display());
         }
         println!("done configuring Windows bundle");
         Ok(())
@@ -115,144 +163,6 @@ impl WindowsBuilder {
         Ok(())
     }
 }
-
-
-
-
-// pub fn convert_png_to_ico(session: &Session, input_path: &str) -> io::Result<()> {
-//     let windows = "windows_icon.ico";
-//     let win_output_path = format!(
-//         "{}/{}/assets/resources/icons/{}",
-//         session.projects_path.as_ref().unwrap(),
-//         session.current_project.as_ref().unwrap(),
-//         windows
-//     );
-//     // Open the PNG file
-//     let img = image::open(input_path).map_err(|e| {
-//         io::Error::new(
-//             io::ErrorKind::Other,
-//             format!("Failed to open {}: {}", input_path, e),
-//         )
-//     })?;
-
-//     // Resize to the specified size
-//     let resized = imageops::resize(&img, 64, 64, imageops::FilterType::Lanczos3);
-//     let resized_img = DynamicImage::ImageRgba8(resized);
-
-//     // Write as ICO
-//     let file = std::fs::File::create(win_output_path.clone())?;
-//     let mut writer = std::io::BufWriter::new(file);
-//     let encoder = image::codecs::ico::IcoEncoder::new(&mut writer);
-//     encoder
-//         .write_image(
-//             resized_img.as_bytes(),
-//             64,
-//             64,
-//             image::ExtendedColorType::Rgba8,
-//         )
-//         .map_err(|e| {
-//             io::Error::new(
-//                 io::ErrorKind::Other,
-//                 format!("Failed to save {} as ICO: {}", win_output_path, e),
-//             )
-//         })?;
-//     println!(
-//         "Converted {} to ICO ({}x{}) and saved as {}",
-//         input_path, 64, 64, win_output_path
-//     );
-
-//     //check for app.rc and if it exists remove it
-//     let rc = format!(
-//         "{}/{}/app.rc",
-//         session.projects_path.as_ref().unwrap(),
-//         session.current_project.as_ref().unwrap()
-//     );
-//     if Path::new(&rc).exists() {
-//         let output = Command::new("rm").arg(&rc).output().unwrap();
-//         if !output.status.success() {
-//             return Err(io::Error::new(
-//                 io::ErrorKind::Other,
-//                 "could not remove old app.rc: {}",
-//             ));
-//         }
-//     }
-//     //create a new app.rc using absolute path passed in
-//     let ico_path = format!(
-//         "{}/{}/assets/resources/icons/windows_icon.ico",
-//         session.projects_path.as_ref().unwrap(),
-//         session.current_project.as_ref().unwrap()
-//     );
-//     let rc_content = format!(r#"IDI_ICON1 ICON "{}""#, ico_path);
-//     let mut rc_file = File::create(&rc)?;
-//     rc_file.write_all(rc_content.as_bytes())?;
-//     //ensure the file is fully written
-//     rc_file.flush()?;
-//     //explicitly close the file
-//     drop(rc_file);
-//     println!("created resource file: {}", &rc);
-//     let res = format!(
-//         "{}/{}/app.res",
-//         session.projects_path.as_ref().unwrap(),
-//         session.current_project.as_ref().unwrap()
-//     );
-//     println!("rc path: {}", &rc);
-//     println!("res path: {}", &res);
-//     let build_path = format!(
-//         "{}/{}/build.rs",
-//         session.projects_path.as_ref().unwrap(),
-//         session.current_project.as_ref().unwrap()
-//     );
-//     //if a build.rs file exists, first remove it.
-//     if Path::new(&build_path).exists() {
-//         let output = Command::new("rm")
-//             .arg(&build_path)
-//             .output()
-//             .unwrap();
-//         if !output.status.success() {
-//             return Err(io::Error::new(
-//                 io::ErrorKind::Other,
-//                 "could not remove old build.rs",
-//             ));
-//         }
-//     }
-//     //populate the build.rs content
-//     let build_content = format!(
-//         r#"
-//         use std::io;
-
-//         fn main() {{
-//             if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" && std::path::Path::new("{}").exists() {{
-//                 embed_resource::compile("app.rc", embed_resource::NONE)
-//                 .manifest_optional();
-//             }}
-//     }}
-    
-//         "#,
-//         &ico_path
-//     );
-//     //Generate a build.rs file
-//     let mut build_file = fs::File::create(&build_path)?;
-//     build_file.write_all(build_content.as_bytes())?;
-//     build_file.flush()?;
-//     println!("Created Build.rs at {}", &build_path);
-//     //copy windows_icon.ico into a favicon.ico
-//     let output = Command::new("cp")
-//         .args([&win_output_path, &wasm_output_path])
-//         .output()
-//         .unwrap();
-
-//     if !output.status.success() {
-//         return Err(io::Error::new(
-//             io::ErrorKind::Other,
-//             "could not copy favicon: {}",
-//         ));
-//     }
-//     println!(
-//         "copied {} ({}x{}) as {}",
-//         win_output_path, 64, 64, wasm_output_path
-//     );
-//     Ok(())
-// }
 
 
 
